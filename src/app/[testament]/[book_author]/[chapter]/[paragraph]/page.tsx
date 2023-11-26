da2d3ef043591672393f68bf1ec1cf1ec7e40535
jojimanos <manosgeorge7@gmail.com>
2023-11-26 17:49:09 +0200

    imported radix ui theme and made some basic styling in the pages. also created a reusable layout component for easy styling of the components

--- src/app/[testament]/[book_author]/[chapter]/[paragraph]/page.tsx
+++ src/app/[testament]/[book_author]/[chapter]/[paragraph]/page.tsx
@@ -1,3 +1,4 @@
+import LayoutComponent from "@/app/components/layoutComponent";
 import NavigationButton from "@/app/components/navigationButton";
 import React from "react";
 
@@ -6,13 +7,10 @@ const Paragraph = ({ params }: { params: { paragraph: string } }) => {
   const paragraph = ["Hello bible!"];
 
   return (
-    <div>
-      Paragraph {params.paragraph} {paragraph[0]}
-      <NavigationButton
-        href={'/'}
-        text="Back to Landing Screen"
-      />
-    </div>
+      <LayoutComponent>
+        {`Paragraph ${params.paragraph} ${paragraph[0]}`}
+        {<NavigationButton href={"/"} text="Back to Landing Screen" />}
+      </LayoutComponent>
   );
 };
 
--- src/app/[testament]/page.tsx
+++ src/app/[testament]/page.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import NavigationButton from "../components/navigationButton";
+import { Flex, Heading } from "@radix-ui/themes";
+import LayoutComponent from "../components/layoutComponent";
 
 const Testament = ({ params }: { params: { testament: string } }) => {
   // hard coded data for testing purposes
   const books: string[] = ["genesis", "matthew"];
 
   return (
-    <div>
-      Testament {params.testament}
-      <NavigationButton
-        href={`${params.testament}/${books[0]}`}
-        text="Enter Book Route"
-      />
-    </div>
+      <LayoutComponent>
+        {`Testament ${params.testament}`}
+                {
+          <NavigationButton
+            href={`${params.testament}/${books[0]}`}
+            text="Enter Book Route"
+          />
+        }
+      </LayoutComponent>
   );
 };
 
